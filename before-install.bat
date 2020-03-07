681bb6b761fd7873d68d0c242b22dedf4e585515
Mukeshkumar2 <59080342+Mukeshkumar2@users.noreply.github.com>
2020-03-07 20:06:42 +0530

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,1 @@
-REM Install Internet Information Server (IIS). 
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+echo BeforeInstall>> %~dp0/../../../../../executed_proof_file
