a01a1117aa6c0bebb554caaaca1abdffb4643173
Mukeshkumar2 <59080342+Mukeshkumar2@users.noreply.github.com>
2020-03-10 11:56:42 +0530

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,6 +1,4 @@
 REM Install Internet Information Server (IIS). 
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
-aws s3 --region us-east-1 cp s3://mkpbucket/django_scripts/dotnet.bat C:\inetpub\wwwroot
-bat C:\inetpub\wwwroot/dotnet.bat
 © 2020 GitHub, Inc.
